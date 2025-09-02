2fa9b492367ca36c1072a3fee85f90a30326ab1f
Arsh Mittal <arshmittalsitl@gmail.com>
2025-09-02 15:07:25 +0530

    first commit

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
@@ -24,4 +25,5 @@ def login():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
